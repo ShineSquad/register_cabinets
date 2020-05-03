a273d179195e3c9a7cff212ada2c5e9b5a45d7cd
burasovartem <morti.show@gmail.com>
2020-05-03 23:11:25 +0500

    update style

--- scripts/script.js
+++ scripts/script.js
@@ -40,8 +40,21 @@
         [[--title--]]
 **/
 
+window.onload = function() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyDCqEaGyOikDZEihTCzTVPZS2c5IvIE5aU",
+    authDomain: "register-6454b.firebaseapp.com",
+    databaseURL: "https://register-6454b.firebaseio.com",
+    projectId: "register-6454b",
+    storageBucket: "register-6454b.appspot.com",
+    messagingSenderId: "911585335407",
+    appId: "1:911585335407:web:cfa0a85cc97f5b0329159a"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+}
+
 const Software = {
-  //template: "<div><div class='left-block'><table border='1' class='po-table'><tr><td>№</td><td>Имя</td><td>Полное название</td><td>Версия</td><td>Лицензия</td></tr><tr style='display: none;'><td>[[--count--]]</td><td>[[--name--]]</td><td>[[--full_name--]]</td><td>[[--version--]]</td><td>[[--license--]]</td></tr></table></div><div class='right-block'><form class='po-form'><p class='form-title'>Добавить программное обеспечение</p><input type='text' name='name' placeholder='Имя'><input type='text' name='full_name' placeholder='Полное наименование'><input type='text' name='version' placeholder='Версия'><input type='text' name='license' placeholder='Лицензия'><div class='license'><input type='checkbox' onchange='change()' id='cb'><p>Сроки лицензии</p></div><div class='license-date' id='hide' style='display: none;'><p>с</p><input type='date' name='time_start'><p>по</p><input type='date' name='time_end'></div><input type='button' value='Добавить' onclick='addSoftware()'></form></div></div>"
   template:
     `
       <div>
@@ -64,7 +77,7 @@ const Software = {
           </table>
         </div>
         <div class='right-block'>
-          <form class='po-form'>
+          <form class='po-form' onsubmit="return false;">
             <p class='form-title'>Добавить программное обеспечение</p>
             <input type='text' name='name' placeholder='Имя'>
             <input type='text' name='full_name' placeholder='Полное наименование'>
@@ -111,7 +124,7 @@ const Cabinets = {
           </table>
         </div>
         <div class='right-block'>
-          <form class='po-form'>
+          <form class='po-form' onsubmit="return false;">
             <p class='form-title'>Добавить кабинет</p>
             <input type='text' name='number' placeholder='Номер'>
             <input type='text' name='housing' placeholder='Корпус'>
@@ -147,7 +160,7 @@ const Subjects = {
         </table>
       </div>
       <div class='right-block'>
-        <form class='po-form'>
+        <form class='po-form' onsubmit="return false;">
           <p class='form-title'>Добавить предмет</p>
           <input type='text' name='title' placeholder='Название'>
           <input type='button' value='Добавить' onclick='addSubjects()'>
@@ -160,19 +173,19 @@ const Reports = {
   template:
   `
     <div>
-      <form class='container-reports'>
+      <form class='container-reports' onsubmit="return false;">
         <select class='' name='subject'>
-          <option disabled>Предмет</option>
+          <option disabled selected>Предмет</option>
           <option>1</option>
           <option>2</option>
         </select>
         <select class='' name='cabinet'>
-          <option disabled>Кабинет</option>
+          <option disabled selected>Кабинет</option>
           <option>1</option>
           <option>2</option>
         </select>
         <select class='' name='software'>
-          <option disabled>Программное обеспечение</option>
+          <option disabled selected>Программное обеспечение</option>
           <option>1</option>
           <option>2</option>
         </select>
@@ -205,17 +218,17 @@ function change() {
 }
 
 function addSoftware() {
-  console.log(1)
+  console.log(1);
 }
 
 function addСabinet() {
-
+  console.log(1);
 }
 
 function addSubjects() {
-
+  console.log(1);
 }
 
 function createReport() {
-
+  console.log(1);
 }
