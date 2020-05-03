0b2d2aa3c7a72d93a28ec023c2ba6dc834d4bf0c
burasovartem <morti.show@gmail.com>
2020-05-03 22:41:27 +0500

    update script

--- scripts/script.js
+++ scripts/script.js
@@ -41,16 +41,145 @@
 **/
 
 const Software = {
-  template: "<div><div class='left-block'><table border='1' class='po-table'><tr><td>№</td><td>Имя</td><td>Полное название</td><td>Версия</td><td>Лицензия</td></tr><tr style='display: none;'><td>[[--count--]]</td><td>[[--name--]]</td><td>[[--full_name--]]</td><td>[[--version--]]</td><td>[[--license--]]</td></tr></table></div><div class='right-block'><form class='po-form'><p class='form-title'>Добавить программное обеспечение</p><input type='text' name='name' placeholder='Имя'><input type='text' name='full_name' placeholder='Полное наименование'><input type='text' name='version' placeholder='Версия'><input type='text' name='license' placeholder='Лицензия'><div class='license'><input type='checkbox' onchange='change()' id='cb'><p>Сроки лицензии</p></div><div class='license-date' id='hide' style='display: none;'><p>с</p><input type='date' name='time_start'><p>по</p><input type='date' name='time_end'></div><input type='button' value='Добавить' onclick='addSoftware()'></form></div></div>"
+  //template: "<div><div class='left-block'><table border='1' class='po-table'><tr><td>№</td><td>Имя</td><td>Полное название</td><td>Версия</td><td>Лицензия</td></tr><tr style='display: none;'><td>[[--count--]]</td><td>[[--name--]]</td><td>[[--full_name--]]</td><td>[[--version--]]</td><td>[[--license--]]</td></tr></table></div><div class='right-block'><form class='po-form'><p class='form-title'>Добавить программное обеспечение</p><input type='text' name='name' placeholder='Имя'><input type='text' name='full_name' placeholder='Полное наименование'><input type='text' name='version' placeholder='Версия'><input type='text' name='license' placeholder='Лицензия'><div class='license'><input type='checkbox' onchange='change()' id='cb'><p>Сроки лицензии</p></div><div class='license-date' id='hide' style='display: none;'><p>с</p><input type='date' name='time_start'><p>по</p><input type='date' name='time_end'></div><input type='button' value='Добавить' onclick='addSoftware()'></form></div></div>"
+  template:
+    `
+      <div>
+        <div class='left-block'>
+          <table border='1' class='po-table'>
+            <tr>
+              <td>№</td>
+              <td>Имя</td>
+              <td>Полное название</td>
+              <td>Версия</td>
+              <td>Лицензия</td>
+            </tr>
+            <tr style='display: none;'>
+              <td>[[--count--]]</td>
+              <td>[[--name--]]</td>
+              <td>[[--full_name--]]</td>
+              <td>[[--version--]]</td>
+              <td>[[--license--]]</td>
+            </tr>
+          </table>
+        </div>
+        <div class='right-block'>
+          <form class='po-form'>
+            <p class='form-title'>Добавить программное обеспечение</p>
+            <input type='text' name='name' placeholder='Имя'>
+            <input type='text' name='full_name' placeholder='Полное наименование'>
+            <input type='text' name='version' placeholder='Версия'>
+            <input type='text' name='license' placeholder='Лицензия'>
+            <div class='license'>
+              <input type='checkbox' onchange='change()' id='cb'>
+              <p>Сроки лицензии</p>
+            </div>
+            <div class='license-date' id='hide' style='display: none;'>
+              <p>с</p>
+              <input type='date' name='time_start'>
+              <p>по</p>
+              <input type='date' name='time_end'>
+            </div>
+            <input type='button' value='Добавить' onclick='addSoftware()'>
+          </form>
+        </div>
+      </div>
+    `
 }
 const Cabinets = {
-  template: "<div><div class='left-block'><table border='1' class='po-table'><tr><td>№</td><td>Номер</td><td>Корпус</td><td>Количество мест</td><td>Доска</td><td>Место преподавателя</td></tr><tr style='display: none;'><td>[[--count--]]</td><td>[[--number--]]</td><td>[[--housing--]]</td><td>[[--number_seats--]]</td><td>[[--board--]]</td><td>[[--workplace--]]</td></tr></table></div><div class='right-block'><form class='po-form'><p class='form-title'>Добавить кабинет</p><input type='text' name='number' placeholder='Номер'><input type='text' name='housing' placeholder='Корпус'><input type='text' name='number_seats' placeholder='Количество посадочных мест'><div class='license'><input type='checkbox' name='workplace'><p>Рабочее место преподавателя</p></div><div class='license'><input type='checkbox' name='board'><p>Доска</p></div><input type='button' value='Добавить' onclick='addСabinet()'></form></div></div>"
+  template:
+    `
+      <div>
+        <div class='left-block'>
+          <table border='1' class='po-table'>
+            <tr>
+              <td>№</td>
+              <td>Номер</td>
+              <td>Корпус</td>
+              <td>Количество мест</td>
+              <td>Доска</td>
+              <td>Место преподавателя</td>
+            </tr>
+            <tr style='display: none;'>
+              <td>[[--count--]]</td>
+              <td>[[--number--]]</td>
+              <td>[[--housing--]]</td>
+              <td>[[--number_seats--]]</td>
+              <td>[[--board--]]</td>
+              <td>[[--workplace--]]</td>
+            </tr>
+          </table>
+        </div>
+        <div class='right-block'>
+          <form class='po-form'>
+            <p class='form-title'>Добавить кабинет</p>
+            <input type='text' name='number' placeholder='Номер'>
+            <input type='text' name='housing' placeholder='Корпус'>
+            <input type='text' name='number_seats' placeholder='Количество посадочных мест'>
+            <div class='license'>
+              <input type='checkbox' name='workplace'>
+              <p>Рабочее место преподавателя</p>
+            </div>
+            <div class='license'>
+              <input type='checkbox' name='board'>
+              <p>Доска</p>
+            </div>
+            <input type='button' value='Добавить' onclick='addСabinet()'>
+          </form>
+        </div>
+      </div>
+    `
 }
 const Subjects = {
-  template: "<div><div class='left-block'><table border='1' class='po-table'><tr><td>№</td><td>Название</td></tr><tr style='display: none;'><td>[[--count--]]</td><td>[[--title--]]</td></tr></table></div><div class='right-block'><form class='po-form'><p class='form-title'>Добавить предмет</p><input type='text' name='title' placeholder='Название'><input type='button' value='Добавить' onclick='addSubjects()'></form></div></div>"
+  template:
+  `
+    <div>
+      <div class='left-block'>
+        <table border='1' class='po-table'>
+          <tr>
+            <td>№</td>
+            <td>Название</td>
+          </tr>
+          <tr style='display: none;'>
+            <td>[[--count--]]</td>
+            <td>[[--title--]]</td>
+          </tr>
+        </table>
+      </div>
+      <div class='right-block'>
+        <form class='po-form'>
+          <p class='form-title'>Добавить предмет</p>
+          <input type='text' name='title' placeholder='Название'>
+          <input type='button' value='Добавить' onclick='addSubjects()'>
+        </form>
+      </div>
+    </div>
+  `
 }
 const Reports = {
-  template: "<form class='container-reports'><select class='' name='subject'><option disabled>Предмет</option><option></option><option></option></select><select class='' name='cabinet'><option disabled>Кабинет</option><option></option><option></option></select><select class='' name='software'><option disabled>Программное обеспечение</option><option></option><option></option></select><input type='button' value='Сформировать' onclick='createReport()'></form>"
+  template:
+  `
+    <div>
+      <form class='container-reports'>
+        <select class='' name='subject'>
+          <option disabled>Предмет</option>
+          <option>1</option>
+          <option>2</option>
+        </select>
+        <select class='' name='cabinet'>
+          <option disabled>Кабинет</option>
+          <option>1</option>
+          <option>2</option>
+        </select>
+        <select class='' name='software'>
+          <option disabled>Программное обеспечение</option>
+          <option>1</option>
+          <option>2</option>
+        </select>
+        <input type='button' value='Сформировать' onclick='createReport()'>
+      </form>
+    </div>
+  `
 }
 
 var router = new VueRouter({
